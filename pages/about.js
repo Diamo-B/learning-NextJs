7a88d17959e115261383bd2b46fb14cdf01f77cf
Diamo-B <diamobachar06@gmail.com>
2022-10-24 23:37:51 +0100

    Added the following: * A relatively empty index page * A relatively empty about page * A relatively empty ninja list page - Two components: A navbar and a Footer

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa maiores officia! Inventore nulla distinctio quisquam assumenda perferendis sequi quas doloremque, magni eveniet autem necessitatibus optio molestiae quaerat iusto voluptatibus.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque corporis provident inventore, quas dignissimos nobis recusandae omnis delectus reprehenderit voluptates? Quia numquam repellat incidunt ex saepe quibusdam minima quod asperiores.</p>
+        </div>
+    );
+}
+ 
+export default About;
